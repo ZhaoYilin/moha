2c523a0c111d396d39f303c319a5cd828cf44b39
Yilin Zhao <zhaoyilin1991@gmail.com>
2021-09-26 13:42:12 +0800

    rewrite the property module

--- moha/property/multipole_moment.py
+++ moha/property/multipole_moment.py
@@ -1,16 +1,113 @@
 import numpy as np
 from moha.io.log import log,timer
-from moha.system.operator import RestrictedOneElectronOperator
-class OneElectronProperty(object):
-    def __init__(self):
-        pass
-    @classmethod
-    def multipole_moment(cls,mol,orbs,wavefunction,lmns,coordinate = np.array([0.,0.,0.])):
-        D = wavefunction.density
+from moha.system.operator import RestrictedMultipoleMomentOperator
+
+class MultipoleMoment(object):
+    """Multipole momnet solver.
+
+    Attributes
+    ----------
+    mol
+        Chemical molecule.
+
+    basis_set
+        Basis set of the molecule
+
+    wfn
+        Hartree Fock wavefunction.
+
+    Methods
+    -------
+    __init__(self,mol,basis_set,ham,wfn)
+        Initialize the solver.
+
+    assign_molecule(self,mol)
+        Assign chemical molecule to the solver.
+
+    assign_basis_set(self,basis_set)
+        Assign basis set to the solver.
+
+    assign_wavefunction(self,wfn)
+        Assign Hartree Fock wavefunction to the solver.
+    """
+    def __init__(self,mol,basis_set,wfn):
+        """Initialize the solver.
+
+        Attributes
+        ----------
+        mol
+            Chemical molecule.
+
+        basis_set
+            Basis set of the molecule
+
+        wfn
+            Hartree Fock wavefunction.
+        """
+        self.assign_molecule(mol)
+        self.assign_basis_set(basis_set)
+        self.assign_wavefunction(wfn)
+
+    def assign_molecule(self,mol):
+        """Assign chemical molecule to the solver.
+
+        Attributes
+        ----------
+        mol
+            Chemical molecule.
+        """
+        self.mol = mol
+
+    def assign_basis_set(self,basis_set):
+        """Assign basis set to the solver.
+
+        Attributes
+        ----------
+        basis_set
+            Basis set of the molecule.
+        """
+        self.basis_set = basis_set
+
+    def assign_wavefunction(self,wfn):
+        """Assign Hartree Fock wavefunction to the solver.
+
+        Attributes
+        ----------
+        wfn
+            Hartree Fock wavefunction.
+        """
+        self.wfn = wfn
+
+    @timer.with_section('Multipole')
+    def kernel(self,lmns,coordinate = np.array([0.,0.,0.])):
+        """Kernel of the solver.
+
+        Returns
+        -------
+        results : dict
+            Multipole moment results.
+        """
+        log.hline()
+        log('Multipole moment Section'.format())
+        log.hline()
+        
+        D = self.wfn.density_matrix
         mms = []
+
         for lmn in lmns:
-            mm_matrix = RestrictedOneElectronOperator.build('multipole_moment',mol,orbs,lmn,coordinate).value
+            mm_matrix = RestrictedMultipoleMomentOperator.build(self.basis_set,lmn,coordinate).integral
             mm = np.dot(D,mm_matrix)
             mm = np.trace(mm,axis1=0,axis2=1)
             mms.append(mm)
-        return mms
+
+        log.hline()
+        log('Multipole Moment'.format())
+        log('{}'.format(mms))
+        log.hline()
+
+        results = {
+        "success": True,
+        "mms": mms
+        }
+        return results
+
--- moha/property/population_analysis.py
+++ moha/property/population_analysis.py
@@ -1,42 +1,236 @@
-import numpy as np
-class PopulationAnalysis(object):
-    """
-    """
-    def __init__(self,charges=[]):
-        self.charges = charges
+from moha.io.log import log,timer
 
-    @classmethod
-    def mulliken(cls,molecule,basis_set,wavefunction,hamiltonian):
+import numpy as np
+
+__all__ = ['PopulationAnalysisMulliken','PopulationAnalysisLowdin']
+
+class PopulationAnalysis(object):
+    """Population Analysis solver.
+
+    Attributes
+    ----------
+    mol
+        Chemical molecule.
+    
+    basis_set
+        Basis set of the molecule
+    
+    ham
+        Chemical Hamiltonian.
+
+    wfn
+        Hartree Fock wavefunction.
+
+    Methods
+    -------
+    __init__(self,mol,basis_set,ham,wfn)
+        Initialize the solver.
+    
+    assign_molecule(self,mol)
+        Assign chemical molecule to the solver.
+    
+    assign_basis_set(self,basis_set)
+        Assign basis set to the solver.
+
+    assign_hamiltonian(self,ham)
+        Assign chemical Hamiltonian to the solver.
+
+    assign_wavefunction(self,wfn)
+        Assign Hartree Fock wavefunction to the solver.
+    """
+    def __init__(self,mol,basis_set,ham,wfn):
+        """Initialize the solver.
+
+        Attributes
+        ----------
+        mol
+            Chemical molecule.
+        
+        basis_set
+            Basis set of the molecule
+            
+        ham
+            Chemical Hamiltonian.
+
+        wfn
+            Hartree Fock wavefunction.
         """
-        Mulliken population analysis
-        D: density matrix
-        S: overlap matrix
+        self.assign_molecule(mol)
+        self.assign_basis_set(basis_set)
+        self.assign_hamiltonian(ham)
+        self.assign_wavefunction(wfn)
+    
+    def assign_molecule(self,mol):
+        """Assign chemical molecule to the solver.
+
+        Attributes
+        ----------
+        mol
+            Chemical molecule.
         """
-        population = cls()
-        S = hamiltonian.operators['overlap'].value
-        D = wavefunction.density
+        self.mol = mol
+    
+    def assign_basis_set(self,basis_set):
+        """Assign basis set to the solver.
+
+        Attributes
+        ----------
+        basis_set
+            Basis set of the molecule.
+        """
+        self.basis_set = basis_set
+
+    def assign_hamiltonian(self,ham):
+        """Assign chemical Hamiltonian to the solver.
+
+        Attributes
+        ----------
+        ham
+            Chemical Hamiltonian.
+        """
+        self.ham = ham
+
+    def assign_wavefunction(self,wfn):
+        """Assign Hartree Fock wavefunction to the solver.
+
+        Attributes
+        ----------
+        wfn
+            Hartree Fock wavefunction.
+        """
+        self.wfn = wfn
+
+class PopulationAnalysisMulliken(PopulationAnalysis):
+    """Mulliken population analysis.
+    
+    Attributes
+    ----------
+    mol
+        Chemical molecule.
+    
+    basis_set
+        Basis set of the molecule
+    
+    ham
+        Chemical Hamiltonian.
+
+    wfn
+        Hartree Fock wavefunction.
+
+    Methods
+    -------
+    __init__(self,mol,basis_set,ham,wfn)
+        Initialize the solver.
+    
+    assign_molecule(self,mol)
+        Assign chemical molecule to the solver.
+    
+    assign_basis_set(self,basis_set)
+        Assign basis set to the solver.
+
+    assign_hamiltonian(self,ham)
+        Assign chemical Hamiltonian to the solver.
+
+    assign_wavefunction(self,wfn)
+        Assign Hartree Fock wavefunction to the solver.
+    """
+    def __init__(self,mol,basis_set,ham,wfn):
+        super().__init__(mol,basis_set,ham,wfn)
+
+    @timer.with_section('Population')
+    def kernel(self):
+        """Kernel of the solver.
+        
+        Returns
+        -------
+        results : dict
+            Mulliken population analysis results.
+        """
+        log.hline()
+        log('Population Analysis Section'.format())
+        log.hline()
+
+        S = self.ham.operators['overlap'].integral
+        D = self.wfn.density_matrix
         if type(D) is np.ndarray:
             DS = np.dot(D,S)
         elif type(D) is dict:
             DS = np.dot(D['alpha'],S) + np.dot(D['beta'],S)
-        for i,atom in enumerate(molecule.atoms):
+        
+        population = []
+
+        for i,atom in enumerate(self.mol.atoms):
             charge = 0.
-            for j,orbital in enumerate(basis_set.basis):
-                if orbital.atom_index==i:
+            for j,basis in enumerate(self.basis_set.bases):
+                if basis.atom_index==i:
                     charge += DS[j,j]
             charge = atom.number - charge
-            print('{0:2s} {1:3d} {2:4s} {3:5f}'.format('Charge on atom', i, ':', charge))
-            population.charges.append(charge)
-        return population
+            population.append(charge)
 
-    @classmethod
-    def lowdin(cls,molecule,basis_set,wavefunction,hamiltonian):
+        log.hline()
+        log('Mulliken Population'.format())
+        for i,charge in enumerate(population):
+            log('{0:2s} {1:3d} {2:4s} {3:5f}'.format('Charge on atom', i, ':', charge))
+        log.hline()
+        
+        results = {
+        "success": True,
+        "population": population
+        }
+        return results
+
+class PopulationAnalysisLowdin(PopulationAnalysis):
+    """Lowdin population analysis.
+    
+    Attributes
+    ----------
+    mol
+        Chemical molecule.
+    
+    basis_set
+        Basis set of the molecule
+    
+    ham
+        Chemical Hamiltonian.
+
+    wfn
+        Hartree Fock wavefunction.
+
+    Methods
+    -------
+    __init__(self,mol,basis_set,ham,wfn)
+        Initialize the solver.
+    
+    assign_molecule(self,mol)
+        Assign chemical molecule to the solver.
+    
+    assign_basis_set(self,basis_set)
+        Assign basis set to the solver.
+
+    assign_hamiltonian(self,ham)
+        Assign chemical Hamiltonian to the solver.
+
+    assign_wavefunction(self,wfn)
+        Assign Hartree Fock wavefunction to the solver.
+    """
+    def __init__(self,mol,basis_set,ham,wfn):
+        super().__init__(mol,basis_set,ham,wfn)
+
+    @timer.with_section('Population')
+    def kernel(self):
+        """Kernel of the solver.
+        
+        Returns
+        -------
+        results : dict
+            Mulliken population analysis results.
         """
-        Lowdin population analysis
-        """
-        population = cls()
-        D = wavefunction.density
-        S = hamiltonian.operators['overlap'].value
+        log.hline()
+        log('Population Analysis Section'.format())
+        log.hline()
+
+        S = self.ham.operators['overlap'].integral
+        D = self.wfn.density_matrix
         val, vec = np.linalg.eig(S)
         val_minus_half = (np.diag(val**(0.5)))
         X = np.dot(vec,np.dot(val_minus_half,np.transpose(vec)))
@@ -44,12 +238,26 @@ class PopulationAnalysis(object):
             XDX = np.dot(X,np.dot(D,X))
         elif type(D) is dict:
             XDX = np.dot(X,np.dot(D['alpha'],X))+np.dot(X,np.dot(D['beta'],X))
-        for i,atom in enumerate(molecule.atoms):
+        
+        population = []
+        
+        for i,atom in enumerate(self.mol.atoms):
             charge = 0.
-            for j,orbital in enumerate(basis_set.basis):
-                if orbital.atom_index==i:
+            for j,basis in enumerate(self.basis_set.bases):
+                if basis.atom_index==i:
                     charge += XDX[j,j]
             charge = atom.number - charge
-            print('{0:2s} {1:3d} {2:4s} {3:5f}'.format('Charge on atom', i, ':', charge))
-            population.charges.append(charge)
-        return population
+            population.append(charge)
+
+        log.hline()
+        log('Lowdin Population'.format())
+        for i,charge in enumerate(population):
+            log('{0:2s} {1:3d} {2:4s} {3:5f}'.format('Charge on atom', i, ':', charge))
+        log.hline()
+        
+        results = {
+        "success": True,
+        "population": population
+        }
+        return results
+
