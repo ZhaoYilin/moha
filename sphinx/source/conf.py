19d973617fbff02de5b74f2663de4641ab4e962b
ZhaoYilin <zhaoyilin1991@gmail.com>
2022-04-21 10:47:30 +0800

    add ico

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -109,6 +109,11 @@ html_theme_options = {'logo_only': True}
 # of the sidebar.
 html_logo = './_static/moha.png'
 
+# The name of an image file (within the static path) to use as favicon of the
+# docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
+# pixels large.
+html_favicon = "_static/moha.ico"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
